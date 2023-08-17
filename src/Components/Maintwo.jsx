8b005759f1227a75b15c5418eee0de2f351f1eba
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-08-17 09:07:52 +0530

    update

--- src/Components/Maintwo.jsx
+++ src/Components/Maintwo.jsx
@@ -1,57 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
-    MDBCard,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCardImage,
-    MDBBtn,
-    MDBCarousel,
-    MDBCarouselItem,
-    MDBRow,
-    MDBCol
-  } from 'mdb-react-ui-kit';
-import './Mainall.css'
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBCarousel,
+  MDBCarouselItem,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
+import "./Mainall.css";
 
-import axios from 'axios';
+import axios from "axios";
 
-function Maintwo({handleClick}) {
+function Maintwo({ handleClick }) {
+  const [first, setfirst] = useState([]);
 
-    const [first, setfirst] = useState([])
-
-    useEffect(() => {
-      
-        axios.get('https://dummyjson.com/products').then((display)=>{setfirst(display.data.products)})
-    }, [])
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products").then((display) => {
+      setfirst(display.data.products);
+    });
+  }, []);
 
   return (
-    <div className='border'>
-
-<MDBRow className='row-cols-1 row-cols-md-5 g-4'>
-{first.map((display)=>
-<MDBCol>
-<MDBCard  className='h-100'>
-      <MDBCardImage src={display.images[0]}  position='top' alt='...' />
-      <MDBCardBody>
-        <MDBCardTitle>{display.title}</MDBCardTitle>
-        <MDBCardText>
-          {display.brand}
-        </MDBCardText>
-        <MDBCardText>$
-          {display.price}
-        </MDBCardText>
-        <MDBBtn color='warning' onClick={()=>handleClick(display)}>ADD TO CART</MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
-</MDBCol>
-
-)}
-
-</MDBRow>
-
-        
+    <div className="border">
+      <MDBRow className="row-cols-1 row-cols-md-5 g-4">
+        {first.map((display) => (
+          <MDBCol>
+            <MDBCard className="h-100">
+              <MDBCardImage src={display.images[0]} position="top" alt="..." />
+              <MDBCardBody>
+                <MDBCardTitle>{display.title}</MDBCardTitle>
+                <MDBCardText>{display.brand}</MDBCardText>
+                <MDBCardText>${display.price}</MDBCardText>
+                <MDBBtn color="warning" onClick={() => handleClick(display)}>
+                  ADD TO CART
+                </MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        ))}
+      </MDBRow>
     </div>
-  )
+  );
 }
 
-export default Maintwo
+export default Maintwo;
